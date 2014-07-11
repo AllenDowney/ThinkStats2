601e190b63d7b20d8595296356ee6f6b3c4ba631
Allen Downey <downey@allendowney.com>
2014-07-11 16:40:15 -0400

    Working on Chapter 13

--- code/survival.py
+++ code/survival.py
@@ -6,6 +6,7 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
 import numpy as np
+import pandas
 
 import nsfg
 import thinkstats2
@@ -51,9 +52,11 @@ class SurvivalFunction(object):
         return 1 - self.cdf.Prob(t)
 
     def Mean(self):
+        """Mean survival time."""
         return self.cdf.Mean()
 
     def Items(self):
+        """Sorted list of (t, s) pairs."""
         return zip(self.ts, self.ss)
 
     def Render(self):
@@ -63,61 +66,67 @@ class SurvivalFunction(object):
         """
         return self.ts, self.ss
 
-    def MakeHazard(self):
+    def MakeHazard(self, label=''):
         """Computes the hazard function.
 
         sf: survival function
 
         returns: Pmf that maps times to hazard rates
         """
-        ts, ss = self.ts, self.ss
+        ss = self.ss
         lams = {}
-        for i in range(len(ts) - 1):
+        for i, t in enumerate(self.ts[:-1]):
             hazard = (ss[i] - ss[i+1]) / ss[i]
-            lams[ts[i]] = hazard
+            lams[t] = hazard
 
-        return HazardFunction(lams)
-
-    def GetCdf(self):
-        """Cdf of durations from a survival curve.
-
-        returns: Cdf of durations
-        """
-        return self.cdf
+        return HazardFunction(lams, label=label)
 
 
 class HazardFunction(object):
+    """Represents a hazard function."""
 
     def __init__(self, d, label=''):
-        self.d = d
+        """Initialize the hazard function.
+
+        d: dictionary (or anything that can initialize a series)
+        label: string
+        """
+        self.series = pandas.Series(d)
         self.label = label
 
-    def Items(self):
-        """Gets an unsorted sequence of (value, freq/prob) pairs."""
-        return self.d.items()
+    def __getitem__(self, t):
+        return self.series[t]
 
     def Render(self):
         """Generates a sequence of points suitable for plotting.
 
         returns: tuple of (sorted times, hazard function)
         """
-        return zip(*sorted(self.Items()))
+        return self.series.index, self.series.values
 
     def MakeSurvival(self):
         """Makes the survival function.
 
         returns: SurvivalFunction
         """
+        ts = self.series.index
+        ss = (1 - self.series).cumprod()
+        cdf = thinkstats2.Cdf(ts, 1-ss)
+        sf = SurvivalFunction(cdf)
+        return sf
+
+    def MakeSurvival2(self):
+        """Makes the survival function.
+
+        returns: SurvivalFunction
+        """
+        ts = self.series.index
         s = 1.0
-        res = []
-
-        for t, lam in sorted(self.Items()):
-            res.append((t, s))
+        ps = []
+        for lam in self.series:
             s *= 1-lam
+            ps.append(1-s)
 
-        # TODO: should I be adding the last point?
-        ts, ss = zip(*res)
-        ps = 1 - np.asarray(ss)
         cdf = thinkstats2.Cdf(ts, ps)
         sf = SurvivalFunction(cdf)
         return sf
@@ -142,24 +151,6 @@ def ConditionalSurvival(pmf, t0):
     return SurvivalFunction(thinkstats2.Cdf(cond))
 
 
-def PlotSurvival(durations):
-    """Plots survival and hazard curves.
-
-    durations: list of durations
-    """
-    cdf = thinkstats2.Cdf(durations)
-    thinkplot.Cdf(cdf, alpha=0.1)
-    thinkplot.PrePlot(2)
-
-    sf = SurvivalFunction(cdf)
-    thinkplot.Plot(sf, label="S(t)")
-
-    hf = sf.MakeHazard()
-    thinkplot.Plot(hf, label='lam(t)')
-
-    thinkplot.Show(xlabel='t (weeks)')
-
-
 def PlotConditionalSurvival(durations):
     """Plots conditional survival curves for a range of t0.
 
@@ -180,82 +171,120 @@ def PlotConditionalSurvival(durations):
     thinkplot.Show()
 
 
-def PlotHazard(past, current):
+def PlotSurvival(complete):
+    """Plots survival and hazard curves.
+
+    complete: list of complete lifetimes
+    """
+    thinkplot.PrePlot(3, rows=2)
+
+    cdf = thinkstats2.Cdf(complete, label='cdf')
+    sf = SurvivalFunction(cdf, label='survival')
+    print(cdf[13])
+    print(sf[13])
+
+    thinkplot.Plot(sf)
+    thinkplot.Cdf(cdf, alpha=0.2)
+    thinkplot.Config()
+
+    thinkplot.SubPlot(2)
+    hf = sf.MakeHazard(label='hazard')
+    print(hf[39])
+    thinkplot.Plot(hf)
+    thinkplot.Config(ylim=[0, 0.75])
+
+
+def PlotHazard(complete, ongoing):
     """Plots the hazard function and survival function.
 
-    past: list of durations for complete pregnancies
-    current: list of durations for current pregnancies
+    complete: list of complete lifetimes
+    ongoing: list of ongoing lifetimes
     """
-    # plot S(t) based on only past pregnancies
-    cdf = thinkstats2.Cdf(past)
+    # plot S(t) based on only complete pregnancies
+    cdf = thinkstats2.Cdf(complete)
     sf = SurvivalFunction(cdf)
     thinkplot.Plot(sf, label='old S(t)', alpha=0.1)
 
     thinkplot.PrePlot(2)
 
     # plot the hazard function
-    hf = EstimateHazardFunction(past, current)
+    hf = EstimateHazardFunction(complete, ongoing)
     thinkplot.Plot(hf, label='lams(t)', alpha=0.5)
 
     # plot the survival function
     sf = hf.MakeSurvival()
+    sf2 = hf.MakeSurvival2()
+    print(sf.cdf == sf2.cdf)
     thinkplot.Plot(sf, label='S(t)')
     thinkplot.Show(xlabel='t (weeks)')
 
 
-def EstimateHazardFunction(past, current):
+def EstimateHazardFunction(complete, ongoing):
     """Estimates the hazard function by Kaplan-Meier.
 
     http://en.wikipedia.org/wiki/Kaplan%E2%80%93Meier_estimator
 
-    past: list of durations for complete pregnancies
-    current: list of durations for current pregnancies    
+    complete: list of complete lifetimes
+    ongoing: list of ongoing lifetimes
     """
-    # pmf and sf of known pregnancy lengths
-    n = len(past)
-    pmf_past = thinkstats2.Pmf(past)
-    sf_past = SurvivalFunction(thinkstats2.Cdf(pmf_past))
+    # pmf and sf of complete lifetimes
+    n = len(complete)
+    pmf_complete = thinkstats2.Pmf(complete)
+    sf_complete = SurvivalFunction(thinkstats2.Cdf(complete))
 
-    # sf for current pregnancies
-    m = len(current)
-    sf_curr = SurvivalFunction(thinkstats2.Cdf(current))
+    # sf for ongoing lifetimes
+    m = len(ongoing)
+    sf_curr = SurvivalFunction(thinkstats2.Cdf(ongoing))
 
     lams = {}
-    for t, s in sf_past.Items():
-        ended = n * pmf_past[t]
-        ongoing = n * s + m * sf_curr[t]
-        at_risk = ended + ongoing
-        hazard = ended / at_risk
-        lams[t] = hazard
+    for t, s in sf_complete.Items():
+        ended = n * pmf_complete[t]
+        at_risk = ended + n * s + m * sf_curr[t]
+        print t, ended, at_risk
+        lams[t] = ended / at_risk
 
     return HazardFunction(lams)
 
 
-def GetDurations(keep_codes):
-    """Reads pregnancy durations from NSFG data.
+def PlotMarriageData():
+    resp = chap01ex_soln.ReadFemResp()
+    resp.cmmarrhx.replace([9997, 9998, 9999], np.nan, inplace=True)
 
-    """
-    preg = nsfg.ReadFemPreg()
-    print 'Number of pregnancies', len(preg)
+    resp['agemarry'] = (resp.cmmarrhx - resp.cmbirth) / 12.0
+    cdf = thinkstats2.Cdf(resp.agemarry)
+    resp['age'] = (resp.cmintvw - resp.cmbirth) / 12.0
+    cdf = thinkstats2.Cdf(resp.age)
 
-    hist = thinkstats2.Hist(preg.outcome)
-    hist.Print()
+    complete = resp[resp.evrmarry==1].agemarry
+    ongoing = resp[resp.evrmarry==0].age
 
-    # TODO: include other outcomes?
-    durations = preg.query('outcome in keep_codes').prglngth
-    print 'Number of relevant pregnancies', len(durations)
-    return durations
+    hf = survival.EstimateHazardFunction(complete, ongoing)
+    sf = hf.MakeSurvival()
+
+    thinkplot.Plot(hf)
+    thinkplot.Plot(sf)
+    
 
 
 def main():
-    past = GetDurations([1, 3, 4])
-    current = GetDurations([6])
-    PlotHazard(past, current)
-    return
 
-    durations = GetDurations([1, 3, 4])
-    PlotSurvival(durations)
-    PlotConditionalSurvival(durations)
+    preg = nsfg.ReadFemPreg()
+    print 'Number of pregnancies', len(preg)
+
+    complete = preg.query('outcome in [1, 3, 4]').prglngth
+    print 'Number of complete pregnancies', len(complete)
+    ongoing = preg[preg.outcome==6].prglngth
+    print 'Number of ongoing pregnancies', len(ongoing)
+
+    PlotSurvival(complete)
+    thinkplot.Save(root='survival1',
+                   xlabel='t (weeks)')
+
+
+    
+
+    #PlotHazard(complete, ongoing)
+    #PlotConditionalSurvival(complete)
 
 
 if __name__ == '__main__':
