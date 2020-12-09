583a6749b1317bc65bcb501369c3ed64ea4dd8ed
Jake Rickord <jrickord@carthage.edu>
2020-12-09 09:37:03 -0600

    Create Week2RickordJake.py

---
+++ homeworks/Week2RickordJake.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 09:15:26 2020
+
+@author: Jake
+"""
+
+##DSC 530-T302
+##Jake Rickord
+##Week 2 Assignment 1
+
+##Hello World Text
+print("Hello World")
+
+##Addition
+p=1+1
+print(p)
+
+##Subtraction
+o=2-1
+print(o)
+
+##Multiplication
+i=4*3
+print(i)
+
+##Division
+u=6/2
+print(u)
+
+##Concatenation
+y="Hello"
+t=" World"
+r=y+t
+print(r)
+
+##List of 4
+e=[1,2,3,4]
+print(e)
+
+##Append to list
+e.append(5)
+print(e)
+
+##tuple creation of 4
+w=("Hello", "to", "the", "World")
+print(w)
