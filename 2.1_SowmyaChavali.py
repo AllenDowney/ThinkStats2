9783120b726ecdca0aaee9f1ca3cf7ed7d2229ee
sowmyachavali <raghuveeryellapantula@Raghuveers-MBP.home>
2020-12-10 23:15:55 -0500

    committing week2 changes

---
+++ 2.1_SowmyaChavali.py
@@ -0,0 +1,41 @@
+# DSC530
+# Week 2 Assignment - 2.1_SowmyaChavali.py
+# Author Sowmya Chavali
+# 12/10/20
+
+# Display the text “Hello World!"
+print("Hello World")
+
+# Add two numbers together
+result = 3+2
+print("Sum of two numbers:", result)
+
+# Subtract a number from another number
+result = 3-2
+print("Difference of two numbers:", result)
+
+# Multiply two numbers
+result = 3*2
+print("Multiplication of two numbers:", result)
+
+# Divide two numbers
+result = 3/2
+print("Division of two numbers:", result)
+
+# Concatenate two strings together
+FirstName = "Sowmya"
+LastName = "Chavali"
+result = FirstName + LastName
+print("Concatenate two strings together: " +result)
+
+# Create a list of 4 items
+List = [1, 2, 'a', 'b']
+print(List)
+
+# Append an item to your list
+List.append(3)
+print(List)
+
+# Create a tuple with 4 items
+Tuple = ("Hi", "this", "is", "Sowmya")
+print(Tuple)
